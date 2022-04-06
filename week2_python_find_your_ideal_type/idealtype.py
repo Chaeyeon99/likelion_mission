000d451caa02b44cc415eb619f699ca47e3c59b7
Chaeyeon99 <a01094779405@gmail.com>
2022-04-06 19:21:37 +0900

    create week2

---
+++ week2_python_find_your_ideal_type/idealtype.py
@@ -0,0 +1,14 @@
+total_dictionary = {}
+
+while True:
+    question = input("질문을 입력해주세요 : ")
+    if question == "q":
+        break
+    else:
+        total_dictionary[question] = ""
+
+for i in total_dictionary:
+    print(i)
+    answer = input("답변을 입력해주세요 : ")
+    total_dictionary[i] = answer
+print(total_dictionary)
