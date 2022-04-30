8703d884ce90c967a92910c7a6c0634ecf78e889
Chaeyeon99 <a01094779405@gmail.com>
2022-04-30 18:11:15 +0900

    add crawling result file

--- week3_python_crawling/naver_crawling.py
+++ week3_python_crawling/naver_crawling.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime
 
 now = datetime.now()
-print("\n","="*10,now.year, "년 ", now.month, "월 ", now.day, "일 차트 순위!", "="*10, "\n")
+print(datetime.today().strftime("\n========== %Y년 %m월 %d일의 차트 순위! ==========\n"))
 
 url = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EC%9D%8C%EC%9B%90%EC%B0%A8%ED%8A%B8&oquery=%EB%A9%9C%EB%A1%A0%EC%B0%A8%ED%8A%B8&tqi=UrZ0HsprvN8ssK5ZP%2BsssssstVh-314088"
 response = requests.get(url)
@@ -11,8 +11,11 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 results = soup.findAll('div','album_info')
 
-i=0
+music_rank_file = open("musicrank.txt","a")
+
+rank=0
 
 for result in results:
-    i = i+1
-    print(i, "위:", result.get_text())
+    rank += 1
+    music_rank_file.write(str(rank)+"위: "+result.get_text()+"\n")
+    print(rank, "위:", result.get_text())
